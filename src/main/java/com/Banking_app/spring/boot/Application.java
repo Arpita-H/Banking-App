a11bea2866118e066b297eed1a272d6bd08c26b8
Arpita-s-h <arpitash7022@gmail.com>
2024-09-03 00:45:16 +0530

    Add:Swagger UI

--- src/main/java/com/Banking_app/spring/boot/Application.java
+++ src/main/java/com/Banking_app/spring/boot/Application.java
@@ -1,9 +1,29 @@
 package com.Banking_app.spring.boot;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Banking App",
+				description = "Backend Rest APIs for Banking App",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Arpita S H",
+						email = "arpitash7204@gmail.com",
+						url = "https://github.com/Arpita-H/Banking-App"
+				),
+				license = @License(
+						name = "Banking App",
+						url = "https://github.com/Arpita-H/Banking-App"
+				)
+		)
+)
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/Banking_app/spring/boot/controller/UserController.java
+++ src/main/java/com/Banking_app/spring/boot/controller/UserController.java
@@ -2,41 +2,94 @@ package com.Banking_app.spring.boot.controller;
 
 import com.Banking_app.spring.boot.dto.*;
 import com.Banking_app.spring.boot.service.impl.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Controller
 @RequestMapping("/api/user")
+@Tag(name = "User Account Management APIs")
 public class UserController {
 
     @Autowired
     UserService userService;
 
+@Operation(
+        summary = "Create New User Account",
+        description = "Creating a new user and assigning an account ID"
+)
+@ApiResponse(
+        responseCode = "201",
+        description = "Http Status 201 CREATED"
+)
     @PostMapping
     public BankResponse createAccount(@RequestBody UserRequest userRequest){
         return userService.createAccount(userRequest);
     }
-
-    @GetMapping("balanceEnquiry")
+    @Operation(
+            summary = "Balance Enquiry",
+            description = "Given an account number, check how much balance amount the user has"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @GetMapping("/balanceEnquiry")
     public BankResponse balanceEnquiry(@RequestBody EnquiryRequest request) {
         return userService.balanceEnquiry(request);
     }
 
-    @GetMapping("nameEnquiry")
+    @Operation(
+            summary = "Name Enquiry",
+            description = "Given an account number, check to whom the account belongs"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @GetMapping("/nameEnquiry")
     public String nameEnquiry(@RequestBody EnquiryRequest request) {
         return userService.nameEnquiry(request);
     }
-    @PostMapping("credit")
+    @Operation(
+            summary = "Credit",
+            description = "Credit the amount to given account number"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @PostMapping("/credit")
     public BankResponse creditAccount(@RequestBody CreditDebitRequest request) {
         return  userService.creditAccount(request);
     }
 
-    @PostMapping("debit")
+    @Operation(
+            summary = "Debit",
+            description = "Debit the amount from given account number"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @PostMapping("/debit")
     public BankResponse debitAccount(@RequestBody CreditDebitRequest request) {
         return userService.debitAccount(request);
     }
 
-    @PostMapping("transfer")
+    @Operation(
+            summary = "Transfer",
+            description = "Debit the given amount from the sourceAccountNumber & Credit it to destinationAccountNumber"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @PostMapping("/transfer")
     public BankResponse transfer(@RequestBody TransferRequest request) {
         return userService.transfer(request);
     }
